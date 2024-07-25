5ccaa682c4e920b04f32b3e131def4fc43ca02a5
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2024-07-25 16:29:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -19,7 +21,12 @@ function Logo() {
   return <h1> 🌴 Far Away 💼</h1>;
 }
 function Form() {
-  function handleSubmit() {}
+  //Create a piece of state
+  // const [description, setDescription] = useState;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
@@ -34,6 +41,8 @@ function Form() {
           </option>
         ))}
       </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
     </form>
   );
 }
